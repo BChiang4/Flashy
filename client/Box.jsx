59a9e4a57f2ad3363e55ef8d67e29b10d3a2efb2
Brian <chiangbrian4@gmail.com>
2022-08-02 16:08:52 -0700

    made additional changes to styling, adding hover affects, svg for rotate button, adjust to border-box

--- client/Box.jsx
+++ client/Box.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import forwardArrow from '../assets/icons8-forward-100.png';
 import backArrow from '../assets/icons8-back-100.png';
+import flipArrow from '../assets/icons8-rotate-100.png'
 import '../src/styles.scss'
 import Flashcard from "./Flashcard.jsx";
 
 function Box(){
-    // const handleClick = ()=>{
-    //     console.log('clicking');
-    // }
+    // move the state of the flashcard to the upmost parent component
+    const handleClick = ()=>{
+        console.log('clicking');
+    }
     return(
         <div className='Box'>
-            {/* <button className="arrow" onClick={handleClick}>
-                <img src={backArrow} aria-hidden="true"></img>
-            </button>
-            <button className='arrow' onClick={handleClick}>
-                <img src={forwardArrow} aria-hidden="true"></img>
-            </button> */}
             <div className='Flashcard'>
               <Flashcard/>  
+            </div>
+            <div className='Button'>
+                <button className="arrow" onClick={handleClick}>
+                    <img src={backArrow} aria-hidden="true"></img>
+                </button>
+                <button className='flipArrow' onClick={handleClick}>
+                    <img src={flipArrow} aria-hidden='true'/>
+                </button>
+                <button className='arrow' onClick={handleClick}>
+                    <img src={forwardArrow} aria-hidden="true"></img>
+                </button>
             </div>
         </div>
     )
--- client/Flashcard.jsx
+++ client/Flashcard.jsx
@@ -3,27 +3,23 @@ import { useState } from "react";
 import '../src/styles.scss'
 const array = ['Transpile','Runtime',"Object Oriented Programming","CSS"];
 function Flashcard(){
-    const [currCard, setCard] = useState(array[0]);
+    // const [currCard, setCard] = useState(array[0]);
 
-    const handleClickLeft = ()=>{
-        // shuffle to another card within our json data and re render the new card onto the screen 
-        // if the current card is the first card on the deck, move to the last card of the deck
-        if(array.indexOf(currCard) === 0){
-            setCard(array[array.length-1]);
-        }
-        setCard(array[array.indexOf(currCard) - 1])
-    }
+    // const handleClickLeft = ()=>{
+    //     // shuffle to another card within our json data and re render the new card onto the screen 
+    //     // if the current card is the first card on the deck, move to the last card of the deck
+    //     if(array.indexOf(currCard) === 0){
+    //         setCard(array[array.length-1]);
+    //     }
+    //     setCard(array[array.indexOf(currCard) - 1])
+    // }
     
-    const handleClickRight = ()=>{
-        setCard(array[array.indexOf(currCard) + 1])
-    }
+    // const handleClickRight = ()=>{
+    //     setCard(array[array.indexOf(currCard) + 1])
+    // }
     return(
         <div>
-            <p>{currCard}</p>
-            <div className='Button'>
-                <button className="left_btn" onClick={handleClickLeft}>left</button>
-                <button className='right_btn' onClick={handleClickRight}>right</button> 
-            </div>
+            <p>Flashy</p>
         </div>
     )
 }
